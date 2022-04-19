e562ce00b670a6457019d28e7a3dcf570ac2ce0d
imanishi-yuta <imanishi-yuta@dmm.com>
2022-04-19 22:49:10 +0900

    react-routerの導入

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { useRoutes } from "react-router-dom";
+import Account from "./pages/account";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+import Top from "./pages/top"
+
+const App = () => {
+  const element = useRoutes([
+    { path: "/", element: <Top /> },
+    { path: "/signin", element: <SignIn /> },
+    { path: "/signup", element: <SignUp /> },
+    { path: "/account", element: <Account /> },
+  ]);
+
+  return element;
+}
+
+export default App;
