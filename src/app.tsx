24bb30b1dbebd5d4274b039d196f8b07334e2458
imanishi-yuta <imanishi-yuta@dmm.com>
2022-04-19 23:33:44 +0900

    build落ち修正

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { useRoutes } from "react-router-dom";
-import Account from "./pages/account";
-import SignIn from "./pages/signin";
-import SignUp from "./pages/signup";
-import Top from "./pages/top"
+import Account from "@/pages/account";
+import SignIn from "@/pages/signin";
+import SignUp from "@/pages/signup";
+import Top from "@/pages/top"
 
 const App = () => {
   const element = useRoutes([
